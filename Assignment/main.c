6f7c6d8f939b6da25d8fe94f96cc8f4a248a627c
Mamoud Bibo <mahmoudbibo910@gmail.com>
2021-04-19 00:12:01 +0200

    added comments

--- Assignment/main.c
+++ Assignment/main.c
@@ -5,10 +5,10 @@ struct student
 	{
 	    char Student_name[100];
 	    int Student_ID;
-	    int date[3];
+	    int date[3];                        //in the format dd/mm/yy
 	    int Student_score_of_last_year;
 	};
-typedef struct student student;
+typedef struct student student;     //for convenience
 
 int main()
 {
