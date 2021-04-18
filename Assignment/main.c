12d5f6a8fcc5e10c63e000c00f80394a43b9573a
Mahmoud Abd Elbaset <twicxwt@gmail.com>
2021-04-18 23:51:15 +0200

    structure

--- Assignment/main.c
+++ Assignment/main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct student
+	{
+	    char Student_name[1000];
+	    int Student_ID;
+	    int day,month,year;
+	    int Student_score_of_last_year;
+	};
+
 int main()
 {
     printf("Hello world!\n");
