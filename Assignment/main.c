87b3e9e8cbeb9336e11ef43ba8d978b263f7018c
Mamoud Bibo <mahmoudbibo910@gmail.com>
2021-04-19 20:58:22 +0200

    Dynamic Array Finished

--- Assignment/main.c
+++ Assignment/main.c
@@ -16,33 +16,7 @@ void display(student* s,int);
 
 int main()
 {
-    int n=3;
-student* x=create_array(n);
-strcpy(x[0].Student_name,"BIBO");
 
-x[0].Student_ID=1;
-x[0].date[0]=11;
-x[0].date[1]=9;
-x[0].date[2]=2000;
-x[0].Student_score_of_last_year=100;
-
-strcpy(x[1].Student_name,"Ahmed");
-x[1].Student_ID=2;
-x[1].date[0]=3;
-x[1].date[1]=4;
-x[1].date[2]=2002;
-x[1].Student_score_of_last_year=200;
-
-strcpy(x[2].Student_name,"Mohamed");
-x[2].Student_ID=3;
-x[2].date[0]=4;
-x[2].date[1]=5;
-x[2].date[2]=2005;
-x[2].Student_score_of_last_year=300;
-display(x,n);
-printf("\n\n\n");
-x=insert(x,&n,1);
-display(x,n);
     return 0;
 }
 student* create_array(int n)
