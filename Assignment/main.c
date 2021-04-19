157b182e52cd8f461c714dc0ce4644e94d9cce78
mahmoud hesham <mahmoud.thebat2000@gmail.com>
2021-04-19 11:56:33 +0200

    added a function to define a student

--- Assignment/main.c
+++ Assignment/main.c
@@ -10,6 +10,19 @@ struct student
 	};
 typedef struct student student;     //for convenience
 
+student create_student(char name[], int ID, int day, int month, int year, int score)
+{
+	// a function to define a student.
+	student result;
+	strcpy(result.Student_name, name);
+	result.Student_ID = ID;
+	result.date[0] = day;
+	result.date[1] = month;
+	result.date[2] = year;
+	result.Student_score_of_last_year = score;
+	return result;
+}
+
 int main()
 {
     printf("Hello world!\n");
